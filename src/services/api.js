76d6121dc16dcb3cac59693f04d70f93a1106376
JerryForsberg <jerry.forsberg@neudesic.com>
2025-01-22 10:59:45 -0800

    use multipart/formdata for content:type

--- src/services/api.js
+++ src/services/api.js
@@ -6,15 +6,19 @@ export const getInventory = async () => axios.get(`${API_BASE_URL}/getInventory`
 export const addInventoryItem = async (item) => axios.post(`${API_BASE_URL}/addInventory`, item);
 export const updateInventory = async (id, newQuantity) => axios.put(`${API_BASE_URL}/updateInventory?id=${id}`, { quantity: newQuantity });
 export const getAnalytics = async () => axios.get(`${API_BASE_URL}/getAnalytics`);
-export const uploadFile = async (file) => axios.post(
-    `${API_BASE_URL}/uploadFile`,
-    file,
-    {
-        headers: {
-            'Content-Type': file.type,
-            'File-Name': file.name,
-        },
-    }
-);
+export const uploadFile = async (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    axios.post(
+        `${API_BASE_URL}/uploadFile`,
+        formData,
+        {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'File-Name': file.name,
+            },
+        }
+    );
+}
 
 
