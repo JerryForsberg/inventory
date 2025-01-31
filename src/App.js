430f5b4854b68faaad9b2862341cc3c9e44ebe64
JerryForsberg <jerry.forsberg@neudesic.com>
2025-01-30 19:14:06 -0800

    fix object

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       .get(`${API_BASE_URL}/auth/me`, { withCredentials: true }) // Make a GET request to your backend
       .then(response => {
         console.log('response in app.js: ', response)
-        setUser(response.data.userId); // If successful, store user data in state
+        setUser(response.data.user.userId); // If successful, store user data in state
       })
       .catch(() => {
         setUser(null); // If failed (e.g., no valid session), set user to null
